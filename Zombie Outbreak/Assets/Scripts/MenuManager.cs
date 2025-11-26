e5ec0abcda3395a4115abb59e18020713dbd02e2
Guadalupe <cofrecesgiraldesguadalupe@gmail.com>
2025-11-26 14:21:24 -0300

    escena de creditos

--- Zombie Outbreak/Assets/Scripts/MenuManager.cs
+++ Zombie Outbreak/Assets/Scripts/MenuManager.cs
@@ -3,25 +3,34 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    private const string SCENE_MAIN = "Main";
+    private const string SCENE_CREDITS = "Credits";
+    private const string SCENE_MAIN_MENU = "MainMenu";
+
     public void InitGame()
     {
-        SceneManager.LoadScene("Main");
+        SceneManager.LoadScene(SCENE_MAIN);
     }
 
     public void ShowCredits ()
     {
+        SceneManager.LoadScene(SCENE_CREDITS);
         Debug.Log("Mostrando créditos...");
     }
 
     public void ExitGame()
     {
-        Application.Quit();
-
         Debug.Log("Saliendo del juego");
+        Application.Quit();
     }
 
     public void ShowOptions()
     {
         Debug.Log("Mostrar opciones");
+    }
+
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene(SCENE_MAIN_MENU);
     }
 }
