e4755cb4275d1841590510349d88b3a1e053cc3b
Guadalupe <cofrecesgiraldesguadalupe@gmail.com>
2025-11-27 17:02:43 -0300

    agregar movimiento de camara y zoom

--- Zombie Outbreak/Assets/Scripts/PlayerController.cs
+++ Zombie Outbreak/Assets/Scripts/PlayerController.cs
@@ -3,9 +3,11 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float speed = 5f;
-    [SerializeField] private float jumpHeight = 2f;
+    [SerializeField] private Transform cameraTransform;
+    [SerializeField] private float speed = 2f;
+    [SerializeField] private float jumpHeight = 0.5f;
     [SerializeField] private float gravity = -9.8f;
+    [SerializeField] private bool shouldFaceMoveDirection = false;
 
     private CharacterController controller;
     private Vector2 moveInput;
@@ -37,8 +39,23 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
-        controller.Move(move * speed * Time.deltaTime);
+        Vector3 forward = cameraTransform.forward;
+        Vector3 right = cameraTransform.right;
+
+        forward.y = 0;
+        right.y = 0;
+
+        forward.Normalize();
+        right.Normalize();
+
+        Vector3 moveDirection = forward * moveInput.y + right * moveInput.y;
+        controller.Move(moveDirection * speed);
+
+        if(shouldFaceMoveDirection && moveDirection.sqrMagnitude > 0.001f)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
+            transform.rotation = Quaternion.Slerp(transform.rotation, toRotation, 10f * Time.deltaTime);
+        }
 
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
